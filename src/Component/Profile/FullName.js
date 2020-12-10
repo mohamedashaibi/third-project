858287fe1eabca1cbd0342a38626266ea49ac445
yomj1 <mohashaibi1@gmail.com>
2020-12-10 16:51:16 +0200

    react component checkpoint

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FullName = () => {
+    return <h2>
+        Mohamed Ahmed Ashaibi
+    </h2>;
+}
+
+export default FullName;
---
+++ src/Component/Profile/ProfileImage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import profileImage from '../../profile-image.png'; 
+
+const ProfileImage = () =>{
+    return <img src={profileImage} alt=""/>;
+}
+
+export default ProfileImage;
